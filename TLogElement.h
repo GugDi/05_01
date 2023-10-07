7b006cdc71d752770e261e7275d1679641770aaf
HugDi <113516550+GugDi@users.noreply.github.com>
2023-10-07 07:28:01 +0400

    TLogElement.h - class TLogElement - public

--- TLogElement.h
+++ TLogElement.h
@@ -11,6 +11,12 @@ protected:
 	virtual void calc() = 0;
 	bool getIn2() { return FIn2; }
 	void setIn2(bool newIn2);
+public:
+	bool getIn1() { return FIn1; }
+	void setIn1(bool newIn1);
+	bool getRes() { return FRes; }
+	TLogElement();
+	void Link(TLogElement* nextElement, int nextIn = 1);
 };
 class TNot : public TLogElement{};
 class TLog2In : public TLogElement{};
