d72e3e8d005c4cc47cd96c53de933c910781e4a4
HugDi <113516550+GugDi@users.noreply.github.com>
2023-10-07 07:26:55 +0400

    TLogElement.h - class TLogElement - protected

--- TLogElement.h
+++ TLogElement.h
@@ -6,6 +6,11 @@ private:
 	bool FIn1, FIn2;
 	TLogElement* FNextEl;
 	int FNextIn;
+protected:
+	bool FRes;
+	virtual void calc() = 0;
+	bool getIn2() { return FIn2; }
+	void setIn2(bool newIn2);
 };
 class TNot : public TLogElement{};
 class TLog2In : public TLogElement{};
