f0d0130787479ce4a921fb19ea721570e4a118e1
HugDi <113516550+GugDi@users.noreply.github.com>
2023-10-07 06:51:21 +0400

    Создание лог элементов в TLogElement.cpp

--- TLogElement.cpp
+++ TLogElement.cpp
@@ -1,1 +1,8 @@
 #include "TLogElement.h"
+TLogElement::TLogElement(){}
+void TLogElement::setIn1(bool newIn1){}
+void TLogElement::setIn2(bool newIn2){}
+void TLogElement::Link(TLogElement* nextElement, int nextIn){}
+void TNot::calc(){}
+void TAnd::calc(){}
+void TOr::calc(){}
