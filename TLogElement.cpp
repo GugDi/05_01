3c071dce0eda6d332b1e555a30d32abddfd6d0e1
HugDi <113516550+GugDi@users.noreply.github.com>
2023-10-07 07:03:04 +0400

    TLogElement.cpp - setIn1(bool newIn1) переменные

--- TLogElement.cpp
+++ TLogElement.cpp
@@ -1,10 +1,17 @@
 #include "TLogElement.h"
-TLogElement::TLogElement()
-{
+TLogElement::TLogElement(){
 	//FNextEl = NULL;
 	FNextEl = 0;
 }
-void TLogElement::setIn1(bool newIn1){}
+void TLogElement::setIn1(bool newIn1){
+	FIn2 = newIn2;
+	calc();
+	if (FNextEl)
+		switch (FNextIn) {
+		case 1: FNextEl->setIn1(getRes()); break;
+		case 2: FNextEl->setIn2(getRes()); break;
+		}
+}
 void TLogElement::setIn2(bool newIn2){}
 void TLogElement::Link(TLogElement* nextElement, int nextIn){}
 void TNot::calc(){}
